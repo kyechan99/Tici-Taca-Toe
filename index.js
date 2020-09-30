310a4cf2f57848405ca2f36566a1ad880e29771b
YeChan Kang <kyechan99@gmail.com>
2020-09-30 05:48:45 +0000

    Room Chat

--- index.js
+++ index.js
@@ -8,22 +8,33 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
-app.get('/game', (req, res) => {
+app.get('/:roomCode', (req, res) => {
 	res.sendFile(__dirname + '/game.html');
 });
 
 io.on('connection', (socket) => {
 	socket.broadcast.emit('hi');
-	console.log('a user connected');
-	socket.on('chat message', (msg) => {
+	console.log(socket.id + ' user connected');
+	
+	socket.on('joinRoom', (roomCode) => {
+		socket.join(roomCode);
+		console.log(socket.id + ' join in ' + roomCode);
+		// socket.sockets.clients(roomCode);io.sockets.clients('room');
+		// console.log(roomCode + ' users :  ' + io.sockets.clients(roomCode));
+	});
+	socket.on('chat message', (roomCode, msg) => {
 		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		io.to(roomCode).emit('chat message', msg);
 	});
 	socket.on('boardClick', (msg) => {
 		console.log('boardClick: ' + msg);
 		io.emit('boardClick', msg);
 	});
+	socket.on('disconnect', function() {
+		console.log(socket.id + ' disconnected..');
+	});
 });
+
 
 http.listen(3000, () => {
 	console.log('listening on *:3000');
--- public/static/index.js
+++ public/static/index.js
@@ -1,46 +1,18 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		BOARD_SIZE: 3,
 		myName: 'U0',
-		myTurn: true,
-		beforeArea: 0
 	},
 	created() {
-		this.$socket = io();
-		this.$socket.on('chat message', (msg)=> {
-			$('#messages').append($('<li>').text(msg));
-		})
-		this.$socket.on('boardClick', (msg) => {
-			$('#messages').append($('<li>').text(msg));
-			$('#' + msg).removeClass('btn-primary');
-			$('#' + msg).addClass('btn-brown');
-			if (this.beforeArea !== 0) {
-				$('#board-area-' + this.beforeArea).removeClass('targeting');
-			}
-			this.beforeArea = msg.substring(3,4);
-			$('#board-area-' + this.beforeArea).addClass('targeting');
-			for (let i = 1; i < this.BOARD_SIZE * this.BOARD_SIZE + 1; i++) {
-				console.log(i);
-				if (Number(this.beforeArea) !== i) {
-					$('#board-area-' + i).addClass('disabled');
-				} else {
-					$('#board-area-' + i).removeClass('disabled');
-				}
-			}
-		});
 	},
 	methods: {
-		boardClick(msg) {
-			this.$socket.emit('boardClick', msg);
-			$('#m').val('');
-		},
-		boardAreaNum(n, m) {
-			return (n - 1) * this.BOARD_SIZE + m;
-		},
-		chatSubmit(msg) {
-			this.$socket.emit('chat message', $('#m').val());
-			$('#m').val('');
+		generateRandomRoomCode() {
+			var letters = '0123456789ABCDEF';
+			var roomCode = '';
+			for (var i = 0; i < 6; i++) {
+			  roomCode += letters[Math.floor(Math.random() * 16)];
+			}
+			return roomCode;
 		}
-  }
+	}
 })
