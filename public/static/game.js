f716d64a27e79760abe93f9d94d6c1711c5768a6
YeChan Kang <kyechan99@gmail.com>
2020-10-01 05:52:18 +0000

    Game Logic

--- public/static/game.js
+++ public/static/game.js
@@ -47,17 +47,30 @@ var app = new Vue({
 			// Remove before board-area targeting animation
 			if (this.beforeArea !== 0)
 				$('#board-area-' + this.beforeArea).removeClass((this.myTurn ? 'my' : 'enemy') + '-targeting');
+			else
+				$('#game-col').removeClass((this.myTurn ? 'my' : 'enemy')+'-targeting');
 			
-			// Show current board-area targeting animation
 			this.beforeArea = msg.substring(3,4);
-			$('#board-area-' + this.beforeArea).addClass((this.myTurn ? 'enemy' : 'my')+'-targeting');
-			
-			// Disabled for non-target board-area
-			for (let i = 1; i < this.BOARD_SIZE * this.BOARD_SIZE + 1; i++) {
-				if (Number(this.beforeArea) !== i) {
-					$('#board-area-' + i).addClass('disabled');
-				} else {
-					$('#board-area-' + i).removeClass('disabled');
+			if (this.boardData[Number(this.beforeArea)-1].owner === undefined) {
+				// Show current board-area targeting animation
+				$('#board-area-' + this.beforeArea).addClass((this.myTurn ? 'enemy' : 'my')+'-targeting');
+
+				// Disabled for non-target board-area
+				for (let i = 1; i < this.BOARD_SIZE * this.BOARD_SIZE + 1; i++) {
+					if (Number(this.beforeArea) !== i) {
+						$('#board-area-' + i).addClass('disabled');
+					} else {
+						$('#board-area-' + i).removeClass('disabled');
+					}
+				}
+			} else {
+				this.beforeArea = 0;
+				$('#game-col').addClass((this.myTurn ? 'enemy' : 'my')+'-targeting');
+				// Open all board-area except where the owner has.
+				for (let i = 1; i < this.BOARD_SIZE * this.BOARD_SIZE + 1; i++) {
+					if (this.boardData[i-1].owner === undefined) {
+						$('#board-area-' + i).removeClass('disabled');
+					}
 				}
 			}
 			
@@ -71,10 +84,17 @@ var app = new Vue({
 			if (firstSocketId === this.$socket.id) {
 				this.myTurn = true;
 				this.myTurnMsg();
+				$('#game-col').addClass((this.myTurn ? 'my' : 'enemy')+'-targeting');
 			} else {
 				this.myTurn = false;
 				this.enemyTurnMsg();
 			}
+		});
+		
+		this.$socket.on('game end', () => {
+			$('#board-area-' + this.beforeArea).removeClass((this.myTurn ? 'my' : 'enemy') + '-targeting');
+			$('#game-col').removeClass((this.myTurn ? 'my' : 'enemy')+'-targeting');
+			this.myTurn = false;
 		});
 		
 		// Out room (Called when the room is full)
@@ -143,6 +163,14 @@ var app = new Vue({
 			$('#messages').append($('<li class="system-msg">Now <span class="badge badge-brown">enemy</span> Turn !</li>'));
 		},
 		/**
+		 * systemMsg
+		 * @desc : System Message
+		 * @param msg : message content
+		 */
+		systemMsg(msg) {
+			$('#messages').append($('<li class="system-msg">').text(msg));
+		},
+		/**
 		 * writeBoardData
 		 * @desc : Write Board data about who's owner about board (who clicked it)
 		 * @param bPos : board position data
@@ -153,6 +181,7 @@ var app = new Vue({
 			let m = Number(bPos.substr(2, 1));
 			this.boardData[boardAreaNum].boards[n+':'+m] = this.myTurn ? 'ME' : 'ENEMY';
 			this.checkBoardOwner(boardAreaNum, n, m);
+			console.log(this.boardData);
 		},
 		/**
 		 * checkBoardOwner
@@ -162,38 +191,140 @@ var app = new Vue({
 		 * @param m : pos-x
 		 */
 		checkBoardOwner(boardAreaNum, n, m) {
+			let owner = this.myTurn ? 'ME' : 'ENEMY';
 			let countMyBlock = 0;
 			for (let i = 1; i < this.BOARD_SIZE+1; i++) {
 				// Horizontal Check
 				countMyBlock = 0;
 				for (let j = 1; j < this.BOARD_SIZE+1; j++) {
-					if (this.boardData[boardAreaNum].boards[i+':'+j] === 'ME')
-						countMyBlock++;
+					if (this.boardData[boardAreaNum].boards[i+':'+j] === owner) countMyBlock++;
 					else break;
 				}
 				
 				// Check if it is finished
-				if (countMyBlock === this.BOARD_SIZE)
-					break;
+				if (countMyBlock === this.BOARD_SIZE) break;
 				
 				// Vertical Check
 				countMyBlock = 0;
 				for (let j = 1; j < this.BOARD_SIZE+1; j++) {
-					if (this.boardData[boardAreaNum].boards[j+':'+i] === 'ME')
-						countMyBlock++;
+					if (this.boardData[boardAreaNum].boards[j+':'+i] === owner) countMyBlock++;
 					else break;
 				}
 				
 				// Check if it is finished
-				if (countMyBlock === this.BOARD_SIZE)
-					break;
+				if (countMyBlock === this.BOARD_SIZE) break;
+			}
+			
+			// Diagonal check (+,+)
+			if (countMyBlock !== this.BOARD_SIZE) {
+				countMyBlock = 0;
+				for (let j = 1; j < this.BOARD_SIZE+1; j++) {
+					if (this.boardData[boardAreaNum].boards[j+':'+j] === owner) countMyBlock++;
+					else break;
+				}
+			}
+			// Diagonal check (-,-)
+			if (countMyBlock !== this.BOARD_SIZE) {
+				countMyBlock = 0;
+				for (let j = 1; j < this.BOARD_SIZE +1; j++) {
+					if (this.boardData[boardAreaNum].boards[j+':'+(this.BOARD_SIZE-j+1)] === owner) countMyBlock++;
+					else break;
+				}
 			}
 			
 			// If board is finished. board-area(big board) is mine.
 			if (countMyBlock === this.BOARD_SIZE) {
-				this.boardData[boardAreaNum].owner = 'ME';
-				$('#messages').append($('<li class="system-msg">').text(boardAreaNum + ' is done.'));
+				console.log(countMyBlock);
+				this.boardData[boardAreaNum].owner = owner;
+				$('#messages').append($('<li class="system-msg">').text(boardAreaNum + 1 + '\s block is done.'));
+				this.checkWinner(owner);
 			}
+			
+			// Check no one can't get board-area
+			countMyBlock = true;
+			for (let prop in this.boardData[boardAreaNum].boards) {
+				if (this.boardData[boardAreaNum].boards[prop] === undefined) {
+					countMyBlock = false;
+					break;
+				}
+			}
+			if (countMyBlock) {
+				this.boardData[boardAreaNum].owner = 'NOONE';
+				$('#messages').append($('<li class="system-msg">').text(boardAreaNum + 1 + '\s block is no-one.'));
+				this.checkWinner(owner);
+			}
+		},
+		/**
+		 * checkWinner
+		 * @desc : Check Who is winner
+		 * @param boardAreaNum : board-area index
+		 * @param n : pos-y
+		 * @param m : pos-x
+		 */
+		checkWinner(owner) {
+			console.log('checkwinner ' + owner);
+			let countBoardOwner = 0;
+			for (let i = 0; i < this.BOARD_SIZE; i++) {
+				// Horizontal Check
+				for (let j = 0; j < this.BOARD_SIZE; j++) {
+					if (this.boardData[(i * this.BOARD_SIZE) + j].owner === owner) countBoardOwner++;
+					else break;
+				}
+				if (countBoardOwner === this.BOARD_SIZE) {
+			console.log('here 0  ');
+					this.gameEnd();
+					return;
+				}
+				
+				// Vertical Check
+				countBoardOwner = 0;
+				for (let j = 0; j < this.BOARD_SIZE; j++) {
+					if (this.boardData[i + (j * this.BOARD_SIZE)].owner === owner) countBoardOwner++;
+					else break;
+				}
+				if (countBoardOwner === this.BOARD_SIZE) {
+			console.log('here 1  ');
+					this.gameEnd();
+					return;
+				}
+			}
+			
+			// Diagonal check (+,+)
+			countBoardOwner = 0;
+			for (let i = 0; i < this.BOARD_SIZE; i++) {
+				if (this.boardData[0 + (i * (this.BOARD_SIZE+1))].owner === owner) countBoardOwner++;
+				else break;
+			}
+			if (countBoardOwner === this.BOARD_SIZE) {
+			console.log('here 2  ');
+				this.gameEnd();
+				return;
+			}
+			
+			// Diagonal check (-,-)
+			countBoardOwner = 0;
+			for (let i = 0; i < this.BOARD_SIZE; i++) {
+				if (this.boardData[(this.BOARD_SIZE-1) + 2 * i].owner === owner) countBoardOwner++;
+				else break;
+			}
+			if (countBoardOwner === this.BOARD_SIZE) {
+			console.log('here 3  ');
+				this.gameEnd();
+				return;
+			}
+		},
+		/**
+		 * gameEnd
+		 * @desc : Game End
+		 */
+		gameEnd() {
+			this.$socket.emit('game end');
+			this.systemMsg('<=== GAME END ===>');
+			
+			if (this.myTurn)
+				$('#messages').append($('<li class="system-msg"><span class="badge badge-primary">your</span> win !</li>'));
+			else
+				$('#messages').append($('<li class="system-msg"><span class="badge badge-brown">enemy</span> win !</li>'));
 		}
 	}
 })
